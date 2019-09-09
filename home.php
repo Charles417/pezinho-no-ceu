7942b61cd5d6bd4ba295d8735621c4fcf9ee051c
victor.gbsantos <geovanevictor375@gmail.com>
2019-09-09 11:51:03 -0300

    colocando o menu na tela inicial

---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Farmácia</title>
+    <link rel="stylesheet" href="cadastros/css/reset.css">
+    <link rel="stylesheet" href="cadastros/css/geral.css">
+</head>
+<body>  
+
+    <nav>
+        <a href="" class="logotipo">Logo</a>
+        <div class="busca">
+            <input type="text" name="q" id="busca">
+            <input type="submit" name="busca" id="busca" value="Busca">
+        </div>
+        <div>
+            <a href="">Sobre</a>
+            <a href="">Login</a>
+            <a href="">Cadastro</a>
+        </div>
+    </nav>
+
+</body>
+</html>
