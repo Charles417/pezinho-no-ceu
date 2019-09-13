09c699844264bd339651ada97144ed718674ba60
victor.gbsantos <geovanevictor375@gmail.com>
2019-09-13 09:31:47 -0300

    adionando slider, contatos e produtos

--- home.php
+++ home.php
@@ -6,7 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Farmácia</title>
     <link rel="stylesheet" href="cadastros/css/reset.css">
+    <link rel="stylesheet" href="cadastros/css/bootstrap.css">
     <link rel="stylesheet" href="cadastros/css/geral.css">
+    
 </head>
 <body>  
 
@@ -19,9 +21,130 @@
         <div>
             <a href="">Sobre</a>
             <a href="">Login</a>
-            <a href="">Cadastro</a>
+            <a href="http://localhost:8080/pezinho-no-ceu/cadastros/cadastro-usuario/cadastro-usuario.php">Cadastro</a>
         </div>
     </nav>
 
+    <header>
+        <div class="contato">
+            <section><a class="conteudo"><img src="cadastros/imagens/tel.png" alt="">(16)996588569 / 996588569</a></section>
+            <section><a class="conteudo"><img src="cadastros/imagens/mail.png" alt="">texeira.mariafumaça@gmail.com</a></section>
+            <section><a class="conteudo"><img src="cadastros/imagens/face.png" alt=""></a>José Teixeira Junior</section>
+            <section><a class="conteudo"><img src="cadastros/imagens/insta.png" alt=""></a>Texeirinhafazfumaça</section>
+            <section><a class="conteudo"><img src="" alt=""></a></section>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-1"></div>
+            <div id="carouselExampleIndicators" class="carousel slide col-10" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="cadastros/imagens/r1.jpg" class="d-block w-100" alt="Nome do remédio">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="cadastros/imagens/r2.jpg" class="d-block w-100" alt="Nome do remédio">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="cadastros/imagens/r3.jpg" class="d-block w-100" alt="Nome do remédio">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Próximo</span>
+                </a>
+            </div>
+            <div class="col-1"></div>
+        </div>
+    </div>
+
+
+
+    <main>
+        <ul class="produtos">
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+        </ul>
+    </main>
+    <main>
+        <ul class="produtos">
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+        </ul>
+    </main>
+    <main>
+        <ul class="produtos">
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+            <li>
+                <img src="" alt="">
+                <a href="" class="nome"></a>
+                <span class="preco">R$</span>
+            </li>
+        </ul>
+    </main>
+   
+    <script src="cadastros/js/jquery-3.4.1.min.js"></script>
+    <script src="cadastros/js/bootstrap.min.js"></script>
+    <script src="cadastros/js/geral.js"></script>
 </body>
 </html>
