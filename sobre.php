4b4242af187c23d210e700d90965b7de4df06df1
joao.pbsousa <jaoobarbosadks26@gmail.com>
2019-10-01 09:27:16 -0300

    lorem

--- sobre.php
+++ sobre.php
@@ -14,6 +14,23 @@
     <?php require_once "header.php"?>
 </head>
 <body>
-     Página em desenvolvimento...
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet ultricies nisi, ac tempor tortor.
+    Donec at semper sem, in consectetur dolor. Nulla eget elementum sapien. Vivamus lacinia, eros quis efficitur vestibulum,
+    tortor tortor sagittis leo, et convallis nulla lacus id est. Morbi eget malesuada purus, sagittis dictum purus. 
+    Quisque aliquet pellentesque quam, eget pulvinar lorem elementum nec. Aliquam nec fringilla lectus, non vestibulum sapien. 
+    Quisque elementum sapien ligula,
+    quis cursus augue maximus vel. Vestibulum semper turpis a lorem dictum, nec commodo nisl dapibus. 
+    In fringilla urna turpis, vel suscipit tortor convallis ut. Maecenas consequat convallis rhoncus. Phasellus rhoncus magna nec dolor viverra viverra. In quam felis, mattis eu risus eget, egestas ullamcorper leo.
+
+    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed id interdum elit. Mauris aliquam magna ut ante vulputate efficitur. Mauris id mauris purus. Aliquam sodales nisl quis elit viverra semper. Mauris pharetra, nibh non ornare venenatis, odio magna eleifend tortor, sit amet ullamcorper est arcu nec velit. Aliquam lacus turpis, consequat vel placerat venenatis, bibendum ut mauris. Suspendisse potenti. Nullam tortor leo, faucibus ut arcu in, venenatis consectetur ipsum.
+
+    In non ultrices sapien. Aliquam a purus tincidunt felis malesuada tristique. Vestibulum tempor massa vel sapien facilisis, eu viverra odio scelerisque. Curabitur metus tellus, gravida vel leo non, sagittis volutpat neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque nec quam non nibh faucibus hendrerit et eget urna. Nunc lobortis nec neque et laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam ut diam pulvinar, gravida libero quis, luctus dolor. Morbi sed mi quis nibh dignissim ornare ac convallis erat. Donec tincidunt ac mi non vestibulum.
+
+    Cras sodales vehicula ultrices. In at interdum ante, non semper arcu. Integer elementum est at ipsum semper efficitur. Praesent velit arcu, pharetra eu erat eu, varius dignissim nisi. Quisque id dui ac erat dapibus porttitor vitae at justo. Cras vulputate velit vel magna vestibulum porttitor. Nullam arcu ex, facilisis vulputate nisl pretium, blandit rutrum eros. Donec quis ultrices turpis. Quisque feugiat venenatis sapien id condimentum. Nunc imperdiet neque a tortor consectetur pellentesque. Proin lacinia dui massa, at porttitor mauris suscipit a. Vivamus tincidunt orci non ex auctor fermentum.
+
+    Mauris eu est ac ex condimentum maximus ac id nunc. Pellentesque vel congue turpis. Curabitur ut risus eros. In hac habitasse platea dictumst. Aenean augue erat, scelerisque a arcu quis, elementum aliquam lectus. Sed iaculis consectetur sagittis. Pellentesque vel erat mattis, ultricies diam nec, consectetur orci. Integer ut suscipit augue. Praesent ornare scelerisque pulvinar. Vivamus dignissim metus orci, a varius dui vulputate nec. Nam rhoncus porta nisl eget cursus. Duis rhoncus ultricies odio et sagittis. Phasellus dignissim finibus nisl. Praesent accumsan quis augue sed ornare.
+
+</p>
 </body>
 </html>
